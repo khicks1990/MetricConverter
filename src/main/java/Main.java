c2b8dadc44f1afe33d0e1b0a9f8777aa55acdbe0
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 07:02:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+       /**
+          The main method creates an instance of the
+          MetricConverterWindow class, displaying its window.
+       */
+
+       public static void main(String[] args)
+       {
+          new MetricConverter();
+       }
+}
